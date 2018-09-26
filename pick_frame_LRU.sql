7f6e4b94227d658e676a6d5b30acf224dd5f24ae
gfgrillo3 <42618027+gfgrillo3@users.noreply.github.com>
2018-09-26 00:24:05 -0300

    Modificacion Error LRU

--- pick_frame_LRU.sql
+++ pick_frame_LRU.sql
@@ -4,13 +4,20 @@ create or replace function
 pick_frame_LRU() returns int as
 $$
 
-select nro_frame from (
+/*select nro_frame from (
 	select nro_frame, max(last_touch) from bufferpool
 	group by nro_frame
 	order by max desc
 	limit 4 offset 0
 	) as bp
 limit 1 offset 3
+*/
+
+/*LA FUNCION DE ARRIBA NO ME FUNCIONA, SI PUEDEN VEANLO A VER SI A USTEDES SI*/
+
+select nro_frame from bufferpool
+order by last_touch asc
+limit 1 offset 0
 
 
 $$
