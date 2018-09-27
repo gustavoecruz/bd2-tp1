0265dc4edbd512e625cc7f1ee2640b6862ccbf72
Andres Pujadas <Andres Pujadas>
2018-09-26 21:20:23 -0300

    creacion de funcion get_disk_page

---
+++ get_disk_page.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION get_disk_page(nro_page integer) RETURNS integer AS $$
+DECLARE
+    result_frame integer := -1;
+	prev_disk_page integer;
+BEGIN
+    SELECT nro_frame INTO result_frame FROM bufferpool WHERE nro_disk_page = $1;
+	IF result_frame <> -1 THEN
+		RAISE NOTICE 'Lectura desde el buffer. Frame Nro: %', result_frame;
+		return result_frame;
+	END IF;
+	
+	SELECT * INTO result_frame FROM pick_frame_LRU();
+	
+	prev_disk_page := (SELECT nro_disk_page FROM bufferpool WHERE nro_frame = result_frame);
+	
+	INSERT INTO bufferpool (nro_frame, free, dirty, nro_disk_page, last_touch)
+		VALUES (result_frame, FALSE, FALSE, nro_page, current_timestamp);
+	
+	RAISE NOTICE 'Lectura desde el disco. Pagina del disco eliminada del buffer: %', prev_disk_page;
+	
+	return result_frame;
+END;
+$$ LANGUAGE plpgsql;
