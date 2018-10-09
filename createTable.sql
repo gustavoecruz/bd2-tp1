d150d12161386c4f0fc2584970d581d25260cc45
gustavoecruz <42586772+gustavoecruz@users.noreply.github.com>
2018-10-09 12:24:17 -0300

    Correción en los inserts

--- createTable.sql
+++ createTable.sql
@@ -1,4 +1,4 @@
-﻿DROP TABLE IF EXISTS bufferpool;
+drop table if exists bufferpool;
 
 CREATE TABLE bufferpool(
 	nro_frame int,
@@ -9,10 +9,10 @@ CREATE TABLE bufferpool(
 	);
 
 INSERT INTO bufferpool (nro_frame, free, dirty, nro_disk_page, last_touch)
-VALUES (1,FALSE,FALSE,1,current_timestamp);
+VALUES (1,FALSE,FALSE,null,clock_timestamp());
 INSERT INTO bufferpool (nro_frame, free, dirty, nro_disk_page, last_touch)
-VALUES (2,FALSE,FALSE,2,current_timestamp);
+VALUES (2,FALSE,FALSE,null,clock_timestamp());
 INSERT INTO bufferpool (nro_frame, free, dirty, nro_disk_page, last_touch)
-VALUES (3,FALSE,FALSE,3,current_timestamp);
+VALUES (3,FALSE,FALSE,null,clock_timestamp());
 INSERT INTO bufferpool (nro_frame, free, dirty, nro_disk_page, last_touch)
-VALUES (4,FALSE,FALSE,4,current_timestamp);
+VALUES (4,FALSE,FALSE,null,clock_timestamp());
